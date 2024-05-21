03a03ac62678635019f212af402082e3dcc6a4a6
Cufee <vkouzin@outlook.com>
2024-05-21 14:30:53 -0400

    placeholders for list

--- internal/components/list.go
+++ internal/components/list.go
@@ -3,20 +3,31 @@ package components
 import (
 	g "github.com/maragudk/gomponents"
 	h "github.com/maragudk/gomponents/html"
-	b "github.com/willoma/bulma-gomponents"
 )
 
 func ListCard() g.Node {
-	return b.Card(
-		b.CardHeader(
-			b.CardHeaderTitle(
+	return h.Div(
+		h.Class("card has-background-black-ter"),
+		h.Header(
+			h.Class("card-header"),
+			h.P(
+				h.Class("card-header-title"),
 				g.Text("List Name"),
 			),
 		),
 		h.Div(
-			g.Text("Some Text"),
+			h.Class("card-content p-4"),
+			h.Div(
+				// h.Class("content"),
+				h.P(g.Text("Some Text")),
+				h.Time(
+					g.Attr("datetime", "2016-1-1"),
+					g.Text("11:09 PM - 1 Jan 2016"),
+				),
+			),
 		),
-		b.CardFooter(
+		h.Footer(
+			h.Class("card-footer"),
 			h.A(
 				h.Href("#"),
 				h.Class("card-footer-item"),
